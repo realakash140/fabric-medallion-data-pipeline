acc628b9a3de0ae063454c67876ae00c22b5f5c4
Akash Srivastava <143002146+realakash140@users.noreply.github.com>
2026-04-27 23:08:37 +0530

    Update gold_layer.sql

--- sql/gold_layer.sql
+++ sql/gold_layer.sql
@@ -1,1 +1,25 @@
+-- Create Gold tables
+CREATE TABLE gold.fact_table AS
+SELECT * FROM SilverLH.dbo.fact_table;
 
+CREATE TABLE gold.customer_dim AS
+SELECT * FROM SilverLH.dbo.customer_dim;
+
+CREATE TABLE gold.store_dim AS
+SELECT * FROM SilverLH.dbo.store_dim;
+
+CREATE TABLE gold.trans_dim AS
+SELECT * FROM SilverLH.dbo.trans_dim;
+
+-- Aggregation
+CREATE VIEW gold.bank_revenue_summary AS
+WITH outerquery AS (
+    SELECT f.*, d.bank_name
+    FROM gold.fact_table f
+    LEFT JOIN gold.trans_dim d
+    ON f.payment_key = d.payment_key
+)
+SELECT bank_name,
+SUM(total_price) AS aggregated_total_price
+FROM outerquery
+GROUP BY bank_name;
